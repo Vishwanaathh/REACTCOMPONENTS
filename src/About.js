88b920b7939fe23007feb5622bd7f3c9777d4525
Vishwanath <140306677+Vishwanaathh@users.noreply.github.com>
2024-06-27 18:58:29 +0530

    Add files via upload

---
+++ src/About.js
@@ -0,0 +1,4 @@
+function About(){
+    return(<div>This is an about page</div>)
+}
+export default About;
---
+++ src/Home.js
@@ -0,0 +1,4 @@
+function Home(){
+   return(<div>THis is HOME <a href="http://localhost:3001/about">Link</a></div>)
+}
+export default Home;
